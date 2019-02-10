24ff3f7edb15c4a069aaef7952bcc04cdad39d30
haroldEKB <haroldekb@mail.ru>
2019-02-11 03:15:03 +0500

    there was some mistakes

--- android/src/geekbrains/android/AndroidLauncher.java
+++ android/src/geekbrains/android/AndroidLauncher.java
@@ -10,8 +10,6 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		System.setProperty("user.name","\\xD0\\x92\\xD0\\xBB\\xD0\\xB0\\xD0\\xB4\\xD0\\xB8\\xD0\\xBC\\xD0\\xB8\\xD1\\x80");
-
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new MyStarGame(), config);
 	}
